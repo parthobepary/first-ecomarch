78267f0dd894ea6360ff58d7db4b4241c3fc52d3
pallobbiswas <pallobbiswas326@gmail.com>
2022-09-11 15:04:24 +0600

    short by rating done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,48 +2,56 @@
   <div>
     <h1>Welcome to online shopping</h1>
     <div>
-      <button @click="showAll">All</button>
-      <button @click="showAllLowprice(10)">0-10</button>
       <button @click="showAllLowprice(30)">10-30</button>
-    </div>
-    <div class="productscards">
-      <div class="itemcard" v-for="item in products" v-bind:key="item.id">
-        <h2>Name: {{ item.title }}</h2>
-        <img class="image" :src="item.image" alt="" />
-        <p>Price: {{ item.price }}</p>
-        <p>Rating: {{ item.rating.rate }}</p>
-        <p>Count: {{ item.rating.count }}</p>
-        <button class="">Add to card</button>
+      <button @click="showAllLowprice(10)">0-10</button>
+      <button @click="showAll">All</button>
+      <div class="margin">
+        <input v-model="inputValue" type="number" />
+        <button @click="inputSearch">Search</button>
       </div>
+    </div>
+    <div>
+      <productsData :products="shortedData"></productsData>
     </div>
   </div>
 </template>
 
 <script>
+import productsData from "../components/ProductsShow.vue";
 export default {
   name: "HelloWorld",
-
+  components: {
+    productsData,
+  },
   data() {
     return {
       products: [],
       shortedData: [],
+      inputValue: "",
     };
   },
   methods: {
     showAll() {
-      this.axios.get("https://fakestoreapi.com/products").then((res) => {
-        this.products = res.data;
-      });
+      this.shortedData = [...this.products];
     },
     showAllLowprice(value) {
       const valus = this.products;
       const result = valus.filter((element) => element?.price <= value);
-      this.products = result;
+      this.shortedData = [...result];
+    },
+    // search by input
+
+    inputSearch() {
+      const rating = this.inputValue;
+      const valus = this.products;
+      const result = valus.filter((element) => element?.rating?.rate > rating);
+      this.shortedData = [...result];
     },
   },
   mounted() {
     this.axios.get("https://fakestoreapi.com/products").then((res) => {
       this.products = res.data;
+      this.shortedData = [...this.products];
     });
   },
 
---
+++ src/components/ProductsShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="productscards">
+    <div class="itemcard" v-for="item in products" v-bind:key="item.id">
+      <h2>Name: {{ item.title }}</h2>
+      <img class="image" :src="item.image" alt="" />
+      <p>Price: {{ item.price }}</p>
+      <p>Rating: {{ item.rating.rate }}</p>
+      <p>Count: {{ item.rating.count }}</p>
+      <button class="">Add to card</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    products: Array,
+  },
+};
+</script>
